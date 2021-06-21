1ab41bf07cb0a69eb98ec27369d40e19b12336da
Naman <cool.naman.ajmera@gmail.com>
2021-06-21 22:07:03 +0530

    Added: Store the data into list for temperory

--- src/main/java/com/naman/addressbooksystem/service/AddressBookContactService.java
+++ src/main/java/com/naman/addressbooksystem/service/AddressBookContactService.java
@@ -10,36 +10,34 @@ import java.util.List;
 @Service
 public class AddressBookContactService implements IAddressBookContactService {
 
+    private List<AddressBookContact> addressBookContactList=new ArrayList<>();
+
     @Override
     public List<AddressBookContact> getAllContact() {
-        List<AddressBookContact> addressBookContactList = new ArrayList<>();
-        addressBookContactList.add(new AddressBookContact(1,
-                new AddressBookDataDTO("Naman", "Ajmera", "Ajmera Bhawan Chaksu")));
         return addressBookContactList;
     }
 
     @Override
     public AddressBookContact getContactById(int contactId) {
-        AddressBookContact addressBookContact = new AddressBookContact(contactId,
-                new AddressBookDataDTO("Naman", "Ajmera", "Chaksu"));
-        return addressBookContact;
+        return addressBookContactList.get(contactId-1);
     }
 
     @Override
     public AddressBookContact createContact(AddressBookDataDTO addressBookDataDTO) {
-        AddressBookContact addressBookContact = new AddressBookContact(1, addressBookDataDTO);
+        AddressBookContact addressBookContact = new AddressBookContact(addressBookContactList.size()+1, addressBookDataDTO);
+        addressBookContactList.add(addressBookContact);
         return addressBookContact;
     }
 
     @Override
     public AddressBookContact updateContact(int contactId, AddressBookDataDTO addressBookDataDTO) {
         AddressBookContact addressBookContact = new AddressBookContact(contactId, addressBookDataDTO);
+        addressBookContactList.set(contactId-1,addressBookContact);
         return addressBookContact;
     }
 
     @Override
     public void deleteContact(int contactId) {
-        AddressBookContact addressBookContact = new AddressBookContact(contactId, new AddressBookDataDTO("Naman", "Ajmera", "Chaksu"));
-        return;
+        addressBookContactList.remove(contactId-1);
     }
 }
