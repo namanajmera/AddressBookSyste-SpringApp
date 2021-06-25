a1c3af4fdc587237a412571f1bc7436adb9246c5
Naman <cool.naman.ajmera@gmail.com>
2021-06-25 19:10:55 +0530

    Added: Setting the environment variable for username and password.

--- src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
+++ src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
@@ -12,6 +12,7 @@ public class AddressBookSystemApplication {
     public static void main(String[] args) {
         ApplicationContext context= SpringApplication.run(AddressBookSystemApplication.class, args);
         log.info("Address Book App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+        log.info("Address Book User DB User is {}",context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
