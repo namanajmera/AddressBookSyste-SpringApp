1f8e4dbfee5b47788195f4d49c089a0db4f97129
Naman <cool.naman.ajmera@gmail.com>
2021-06-25 18:53:38 +0530

    Added: Add the log info detail

--- src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
+++ src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
@@ -1,13 +1,16 @@
 package com.naman.addressbooksystem;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AddressBookSystemApplication.class, args);
+        log.info("Address Book App Started.");
     }
 
 }
