7dfcf051b996aa7fdf4b6455aee2042ff7815188
Naman <cool.naman.ajmera@gmail.com>
2021-06-25 19:01:42 +0530

    Added: Prod and DEV Properties to create a new environment

--- src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
+++ src/main/java/com/naman/addressbooksystem/AddressBookSystemApplication.java
@@ -3,14 +3,15 @@ package com.naman.addressbooksystem;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class AddressBookSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookSystemApplication.class, args);
-        log.info("Address Book App Started.");
+        ApplicationContext context= SpringApplication.run(AddressBookSystemApplication.class, args);
+        log.info("Address Book App Started in {} Environment",context.getEnvironment().getProperty("environment"));
     }
 
 }
