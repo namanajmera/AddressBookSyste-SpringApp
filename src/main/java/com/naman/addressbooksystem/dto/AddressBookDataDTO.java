f0092487152a3f6e419f24b075f7d417c75acf69
Naman <cool.naman.ajmera@gmail.com>
2021-06-26 14:59:37 +0530

    Modified: Validation in DTO and add the regex in Zip code.

--- src/main/java/com/naman/addressbooksystem/dto/AddressBookDataDTO.java
+++ src/main/java/com/naman/addressbooksystem/dto/AddressBookDataDTO.java
@@ -3,7 +3,6 @@ package com.naman.addressbooksystem.dto;
 import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 public @ToString
@@ -12,10 +11,10 @@ class AddressBookDataDTO {
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Enter the valid firstName")
     public String fullName;
 
-    @NotNull(message = "Address Should not be empty.")
+    @NotBlank(message = "Address Should not be empty.")
     public String address;
 
-    @NotNull(message = "City should not be empty")
+    @NotBlank(message = "City should not be empty")
     public String city;
 
     @NotBlank(message = "State should not be empty")
@@ -24,6 +23,7 @@ class AddressBookDataDTO {
     @Pattern(regexp = "([0]{1}\\s|[+][9]{1}[1]{1}\\s)?[6-9]{1}[0-9]{9}", message = "Enter the valid mobile number with proper format")
     public String phoneNumber;
 
+    @Pattern(regexp = "[1-9]{1}[0-9]{5}", message = "Should be a 6 length and not start with Zero")
     public String zip;
 
     public AddressBookDataDTO(String fullName, String address) {
